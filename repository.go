4f1b8010fbb5115ecbeff08ea7dccf276ac45cb6
Hugo Leonardo Simas Mendes <hmendes00@gmail.com>
2017-07-17 11:37:39 -0400

    Create repository.go

---
+++ repository.go
@@ -0,0 +1,67 @@
+package repomodel
+
+import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var dbName string = "testcooking"
+
+type Repository struct {
+	TableName string
+	DecodeTo  interface{}
+}
+
+func (r *Repository) Insert(t interface{}) error {
+
+	session := OpenSession()
+	defer session.Close()
+
+	c := Table(session, r.TableName)
+	err := c.Insert(&t)
+
+	return err
+}
+
+func Table(s *mgo.Session, tName string) *mgo.Collection {
+	return s.DB(dbName).C(tName)
+}
+
+//params will be bson structs
+func (r *Repository) Update(filter interface{}, t interface{}) error {
+
+	session := OpenSession()
+	defer session.Close()
+
+	c := Table(session, r.TableName)
+
+	err := c.Update(filter, bson.M{"$set": &t})
+
+	return err
+}
+
+func (r *Repository) Select(filter interface{}, take int) (interface{}, error) {
+
+	session := OpenSession()
+	defer session.Close()
+
+	c := Table(session, r.TableName)
+
+	result := make([]interface{}, 0)
+
+	err := c.Find(filter).Limit(take).All(&result)
+
+	return result, err
+}
+
+func OpenSession() *mgo.Session {
+	session, err := mgo.Dial("localhost")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	return session
+}
